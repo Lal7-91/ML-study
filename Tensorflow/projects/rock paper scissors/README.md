08f9e06b5cf4e4806f19852c56ef4c903fd74491
Saleh Ahmed <Saleh.Ahmed.nb8@gmail.com>
2024-10-05 17:22:19 +0300

    All DONE

--- Tensorflow/projects/rock paper scissors/README.md
+++ Tensorflow/projects/rock paper scissors/README.md
@@ -1,3 +1,16 @@
-# Rock Paper Scissors
+## About the Project
+
+Hi, I'm Saleh. I started this challenge about three months ago. Initially, I didn’t do much—just tried some simple sequences, hoping to find a solution without having to write a long code. Fortunately, I didn’t have the right sequence at that time.
+
+However, three days ago, I decided to approach the problem more seriously. I spent around 4 hours writing the algorithm on paper, which really paid off. The implementation itself wasn’t too difficult, though debugging took a bit longer than expected.
+
+Finally, I finished the project, and I’m happy with the result! I hope you like it too.
+
+- Saleh
+
+
+
+### Rock Paper Scissors
 
 This is the boilerplate for the Rock Paper Scissors project. Instructions for building found in https://www.freecodecamp.org/learn/machine-learning-with-python/machine-learning-with-python-projects/rock-paper-scissors
+
--- Tensorflow/projects/rock paper scissors/RPS.py
+++ Tensorflow/projects/rock paper scissors/RPS.py
@@ -1,32 +1,23 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. 
-# It is not a very good player so you will need to change the code to pass the challenge.
-
-
-# def player(prev_play, opponent_history=[]): # stratigy 1, init guss = P and 4 got 2/4
-#     opponent_history.append(prev_play)
-
-#     guess = "P"
-#     if len(opponent_history) > 4:   
-#         guess = opponent_history[-4]
-#     return guess
+#This bot is a combination of all the bots. It will play 35 games to know the player and then it will play the best stratigy to win the game.
 
 import random
 
-def player(prev_play, counter=[0], opponent_history=[], win=[0], bot=[0], char_sequence = []): # stratigy3: test 35 games to know the player
-    
+def player(prev_play, counter=[0], opponent_history=[], win=[0], bot=[0], char_sequence = []):
     opponent_history.append(prev_play)
     
-        
+    # The 35 test games
     if counter[0] < 34:
         
         win[0] = 0    
 
-        if counter[0] >= 0 and counter[0] < 10: #mrugesh checker
+        if counter[0] >= 0 and counter[0] < 10: #mrugesh checker (10 games)
             
+            if counter[0] == 0:
+                print("\nmurgesh check")
             
             if counter[0] == 9: 
                 j = 0
-                theo_hist = ['', 'R', 'R', 'S', 'S', 'S', 'S', 'S', 'S']    
+                theo_hist = ['', 'R', 'R', 'S', 'S', 'S', 'S', 'S', 'S']   # what must murgesh played 
                 for oopPlayer in opponent_history[0:9]:
                     if oopPlayer == theo_hist[j]:
                         win[0]+= 1
@@ -35,7 +26,6 @@ def player(prev_play, counter=[0], opponent_history=[], win=[0], bot=[0], char_s
                 
                         
                 if win[0] == 9:
-                    # counter[0] = 40
                     bot[0] = 1
                     
                 print("abbey check")
@@ -43,12 +33,11 @@ def player(prev_play, counter=[0], opponent_history=[], win=[0], bot=[0], char_s
                     
                     
             counter[0]+= 1
-            # print("P")
             return "P"
         
         
             
-        elif counter[0] >= 10 and counter[0] < 19: # abbey checker            
+        elif counter[0] >= 10 and counter[0] < 19: # abbey checker (9 games)            
             
             plays = ["P", "S", "S", "R", "P", "P", "R", "R", "S"]
             
@@ -56,8 +45,7 @@ def player(prev_play, counter=[0], opponent_history=[], win=[0], bot=[0], char_s
             if counter[0] == 18:
                
                 j = 0
-                # print(f"This is abbey hist:{opponent_history[10:18]}")
-                theo_hist = ['S', 'S', 'S', 'P', 'R', 'S', 'S', 'S'] 
+                theo_hist = ['S', 'S', 'S', 'P', 'R', 'S', 'S', 'S']  # what must abbey played
                    
                 for oopPlayer in opponent_history[10:18]:
                     if oopPlayer == theo_hist[j]:
@@ -66,7 +54,6 @@ def player(prev_play, counter=[0], opponent_history=[], win=[0], bot=[0], char_s
                     j += 1
                         
                 if win[0] == 8:
-                    # counter[0] = 40
                     bot[0] = 2
                     
                 print("quincy check")
@@ -77,7 +64,7 @@ def player(prev_play, counter=[0], opponent_history=[], win=[0], bot=[0], char_s
             
         
         
-        elif counter[0] >= 19 and counter[0] < 25: #quincy checker
+        elif counter[0] >= 19 and counter[0] < 25: #quincy checker (6 games)
                         
             plays = ["P", "P", "S", "S", "R"]
             
@@ -87,9 +74,8 @@ def player(prev_play, counter=[0], opponent_history=[], win=[0], bot=[0], char_s
             
             
             if counter[0] == 24:
-                # print(f"This is quincy hist:{opponent_history[19:24]}")
                 j = 0
-                theo_hist = ['S', 'R', 'R', 'P', 'P']
+                theo_hist = ['S', 'R', 'R', 'P', 'P'] # what must quincy played
                    
                 for oopPlayer in opponent_history[19:24]:
                     if oopPlayer == theo_hist[j]:
@@ -97,18 +83,16 @@ def player(prev_play, counter=[0], opponent_history=[], win=[0], bot=[0], char_s
                     j += 1
                         
                 if win[0] == 5:
-                    # counter[0] = 40
                     bot[0] = 3
                 print("kirs check")
 
             counter[0]+= 1 
         
-            # print(plays[(counter[0]-21)%5])    
             return plays[(counter[0]-21)%5]
             
         
         
-        elif counter[0] >= 25 and counter[0] < 34:  #kris checker            
+        elif counter[0] >= 25 and counter[0] < 34:  #kris checker   (9 games)        
             
             plays = ["P", "S", "R"]
             
@@ -116,7 +100,7 @@ def player(prev_play, counter=[0], opponent_history=[], win=[0], bot=[0], char_s
             if counter[0] == 32:
                 
                 j = 0       
-                theo_hist = ['R', 'P', 'R', 'P', 'S', 'R', 'P', 'S']
+                theo_hist = ['R', 'P', 'R', 'P', 'S', 'R', 'P', 'S'] # what must kris played
                 
                 for oopPlayer in opponent_history[25:32]:
                     if j > 3:
@@ -126,15 +110,14 @@ def player(prev_play, counter=[0], opponent_history=[], win=[0], bot=[0], char_s
                     j += 1
                         
                 if win[0] == 6:
-                    # counter[0] = 40
                     bot[0] = 4
                     
             counter[0]+= 1  
             return plays[(counter[0]-25) % 3]
                 
             
-            
-    if counter[0] == 34:
+    #Here it decides the stratigy to play         
+    if counter[0] == 34:       
         
         if bot[0] == 1:
             print("Bingo I founded you Murgesh!")
@@ -146,22 +129,21 @@ def player(prev_play, counter=[0], opponent_history=[], win=[0], bot=[0], char_s
             print("Bingo I founded you Quincy!")
             
         elif bot[0] == 4:
-            print("Bingo I founded you kirs!")
-            
+            print("Bingo I founded you kirs!")  
         else:
-            print("Sorry I am stupid and nothing worked ):")
+            print("WOW HUMAN YOU PALYED 35 GAMES TO SEE THIS MESSAGE :)")
             
             
             
     if counter[0] >= 34 and counter[0] < 999:
 
-        if bot[0] == 1:             # Murgesh statigy           
+        if bot[0] == 1:             # Murgesh statigy: play in this sequence: P, S, S, R, P, P, R, R, S          
             choices = ["P", "S", "R", "P", "S", "R", "S", "R"]
             counter[0] += 1
             return choices[(counter[0]-35) % len(choices)]  
         
             
-        elif bot[0] == 2:                  #abbey stratigy     
+        elif bot[0] == 2:                  #abbey stratigy: it neeeds to makes abbey sequences equal, then play in equavekent sequence   
             if counter[0] >= 34 and counter[0] < 188:
                      #len is 138  
                 if counter[0] == 34:
@@ -191,14 +173,14 @@ def player(prev_play, counter=[0], opponent_history=[], win=[0], bot=[0], char_s
 
                 return choices[(counter[0] - 188) % len(choices)] 
             
-        elif bot[0] == 3:   #quincy statigy  
+        elif bot[0] == 3:   #quincy statigy: just play oppestie
             counter[0] += 1
             choices = ["P", "P", "S", "S", "R"] 
             return choices[(counter[0]-35) % len(choices)]
             
 
             
-        elif bot[0] == 4:    #kris statigy
+        elif bot[0] == 4:    #kris statigy: near to murgesh but not same
             counter[0] += 1
             choices = ["R", "P", "S", "R", "P", "S", "R", "P"] 
             return choices[(counter[0]-35) % len(choices)]
@@ -209,7 +191,7 @@ def player(prev_play, counter=[0], opponent_history=[], win=[0], bot=[0], char_s
             return random.choice(choices)
             
 
-    else:                        
+    else:    # Every 1000 games it will reset the every thing so PLEASE play in 1000 factors <3          
         print(f"{counter[0] + 1} games done!\n")
         prev_play = ""
         counter[0] = 0
